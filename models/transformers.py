938803b46ad8e1643bcf51cf95011675942cc6e7
ramssesdlsm <rdelossantosm1800@alumno.ipn.mx>
2025-10-07 18:22:51 -0600

    feat(MultiHeadAttention): add implementation of MultiHeadAttention with dropout

---
+++ models/transformers.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, dim: int, num_heads: int, attn_dropout: float = 0.0, out_dropout: float = 0.0):
+        super(MultiHeadAttention, self).__init__()
+        assert dim % num_heads == 0, "The dimension must be divisible by the number of heads"
+
+        self.num_head = num_heads
+        self.head_dim = dim // num_heads
+        self.scale = self.head_dim ** -0.5
+
+        self.to_qkv = nn.Linear(dim, dim * 3, bias=False)
+
+        self.q_bias = nn.Parameter(torch.zeros(dim))
+        self.v_bias = nn.Parameter(torch.zeros(dim))
+        self.register_buffer('k_bias', torch.zeros(dim))
+
+        self.to_out = nn.Linear(dim, dim, bias=True)
+        
+        self.attn_dropout = nn.Dropout(attn_dropout)
+        self.out_dropout = nn.Dropout(out_dropout)
+
+    def forward(self, x: torch.Tensor, mask: torch.Tensor = None) -> torch.Tensor:
+        batch_size, seq_len, _ = x.shape
+
+        qkv = F.linear(input=x, weight=self.to_qkv.weight, bias=torch.cat((self.q_bias, self.k_bias, self.v_bias)))
+
+        # (B, L, 3*dim) -> (B, L, 3, H, HD)
+        qkv = qkv.view(batch_size, seq_len, 3, self.num_head, self.head_dim)
+
+        # (B, L, 3, H, HD) -> (3, B, H, L, HD)
+        qkv = qkv.permute(2, 0, 3, 1, 4)
+
+        q, k, v = qkv[0], qkv[1], qkv[2]  
+
+        attn_scores = torch.matmul(q, k.transpose(-2, -1)) * self.scale
+
+        if mask is not None:
+            mask_value = -torch.finfo(attn_scores.dtype).max
+            attn_scores = attn_scores.masked_fill(~mask.bool(), mask_value)
+
+        attn_weights = attn_scores.softmax(dim=-1)
+        attn_weights = self.attn_dropout(attn_weights)
+
+        out = torch.matmul(attn_weights, v)
+
+        out = out.transpose(1, 2).contiguous().view(batch_size, seq_len, self.dim)
+
+        out = self.out_dropout(self.to_out(out))
+
+        return out
